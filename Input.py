db19a1ea910a59184336de7d52e19eb9550b8691
JayantGoel001 <jgoel92@gmail.com>
2021-04-11 19:38:39 +0530

    Initial Commit

---
+++ Input.py
@@ -0,0 +1,7 @@
+from pywebio.input import *
+from pywebio.output import *
+
+input("What's Your Name?")
+select("Select food", ['Orange', 'Apple'])
+checkbox("Are you Okay?", options=['I am Ok'])
+radio("What Do You like to do?", options=['Eat', 'Sleep', 'Study'])
