48208744c1f8dbaecdfd7a18705e953ffe8f7422
JayantGoel001 <jgoel92@gmail.com>
2021-04-12 06:00:58 +0530

    Initial Commit

--- PDF Generator.py
+++ PDF Generator.py
@@ -2,23 +2,41 @@ from pywebio import *
 from fpdf import FPDF
 
 
-def createPDF(filename, text):
+def createPDF(filename, pages, font, size):
     pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', '', 16)
 
-    lines = text.split("\n")
-    for i in range(len(lines)):
-        pdf.cell(40, 10, lines[i], 0, i + 1)
+    pdf.set_font(font, '', size)
 
-    if filename.split(".")[-1]!="pdf":
-        filename+=".pdf"
+    if filename.split(".")[-1] != "pdf":
+        filename += ".pdf"
+
+    for text in pages:
+        pdf.add_page()
+        lines = text.split("\n")
+        for i in range(len(lines)):
+            pdf.cell(40, 10, lines[i], 0, i + 1)
+
     pdf.output(filename, 'F')
+
 
 def app():
     filename = input.input(placeholder="Name of your file?", required=True)
-    text = input.textarea(placeholder="Type Here......", required=True)
-    createPDF(filename, text)
+    fonts = ['Helvetica', 'Calibri', 'Futura', 'Garamond', 'Times New Roman', 'Arial', 'Cambria', 'Verdana', 'Rockwell']
+    text_info = input.input_group("Text Font And Size", [
+        input.select('Select your font', options=fonts, value='Arial', name='Font'),
+        input.input('Select your text size', value='16', type=input.NUMBER, name='Size')
+    ])
+    add_more = True
+    pages = []
+    while add_more:
+        page = input.textarea(placeholder="Type Here......", required=True)
+        pages.append(page)
+        add_more = input.actions("Would You like to add another page?", buttons=[
+            {'label': 'yes', 'value': True},
+            {'label': 'no', 'value': False}
+        ])
+
+    createPDF(filename, pages, text_info['Font'], text_info['Size'])
     output.put_text("PDF created.")
 
 
